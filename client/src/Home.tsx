e9e37de6ece984587342516be9ba95e47e49a943
Dick Henrik Larsson <dicklarsson@gmail.com>
2022-01-04 12:33:03 +0100

    Adding disclaimer and link to Github

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -35,8 +35,7 @@ export function Home({ user, assets }) {
 
       asset.link = asset.name;
       asset.ipfsURL =
-        asset.ipfs_hash &&
-        "https://ipfs.infura.io/ipfs/" + asset.ipfs_hash;
+        asset.ipfs_hash && "https://ipfs.infura.io/ipfs/" + asset.ipfs_hash;
 
       asset.image = null;
       if (asset.ipfsURL) {
@@ -62,13 +61,39 @@ export function Home({ user, assets }) {
       return asset;
     });
 
-  const items = assets || []; 
+  const items = assets || [];
   return (
     <div className="padding-default">
       <div className="glass raven-rebels-multi-wallet__home-heading padding-default">
         <h2>{settings.heading}</h2>
         <h5>{user.displayName || user.phoneNumber}</h5>
+
+        <ul style={{ background: "black", opacity: "70%", fontSize: "11px" }}>
+          <li>Disclaimer. This is an educational demo of a web wallet</li>
+          <li>
+            Assets only, not actual RVN. Do NOT send RVN to or store anything of
+            value in this wallet
+          </li>
+          <li>
+            Code open source on{" "}
+            <a
+              href="https://github.com/ravenrebels/one-to-many-wallet"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+
+        <ul style={{ background: "black", opacity: "70%", fontSize: "11px" }}>
+          <li>
+            Organisations can setup a web wallet where customers/users can
+            access tokens in seconds. Users can withdraw tokens to their own
+            possession/wallets.
+          </li>
+        </ul>
       </div>
+
       {items.map(function (i) {
         return (
           <div
